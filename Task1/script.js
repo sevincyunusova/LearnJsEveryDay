6bb97152723d91595461f08c1717fbc569f3e1c5
sevincyunusova <yunusovasevinc08@gmail.com>
2026-04-29 17:55:18 +0400

    task 1 finished

--- Task1/script.js
+++ Task1/script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('change-btn');
 const text = document.getElementById('color-name');
 const colors = ['red','green','blue', 'yellow', 'purple', 'orange', 'pink', 'brown', 'cyan', 'magenta'];
 
-btn.addEventListener('click', () => {
+btn.addEventListener('click', ()=>{
     const randomIndex = Math.floor(Math.random() * colors.length);
     const selectedColor = colors[randomIndex];
 
