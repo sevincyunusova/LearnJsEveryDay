374ea26e8c83f8328b9414ce2d652f9662f348c4
sevincyunusova <yunusovasevinc08@gmail.com>
2026-04-29 18:20:28 +0400

    html css js created

---
+++ Task2/script.js
@@ -0,0 +1,16 @@
+const lamp = document.getElementById('lamp');
+
+lamp.style.backgroundColor = "gray";
+document.body.style.backgroundColor = "white";
+
+lamp.addEventListener('click', () => {
+    if (lamp.style.backgroundColor === "gray") {
+        lamp.style.backgroundColor = "yellow";
+        document.body.style.backgroundColor = "black";
+    }
+    else {
+        lamp.style.backgroundColor = "gray";
+        document.body.style.backgroundColor = "white";
+
+    }
+})
