7c90c42929ff3d1dfeeae5b0df81433fef9fda68
sevincyunusova <yunusovasevinc08@gmail.com>
2026-05-17 12:11:58 +0400

    js codes created

---
+++ Task10 - FAQ/main.js
@@ -0,0 +1,15 @@
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(btn){
+    btn.addEventListener('click', function(){
+        const answer = this.nextElementSibling;
+
+        if(answer.style.display === 'block'){
+            answer.style.display = 'none';
+        }
+
+        else{
+            answer.style.display = 'block';
+        }
+    })
+})
