43a86389ce60e8516f8b877583e513ef4c47f752
sevincyunusova <yunusovasevinc08@gmail.com>
2026-05-21 20:04:23 +0400

    eddit

--- Task12 - CaroselInterval/index.js
+++ Task12 - CaroselInterval/index.js
@@ -1,6 +1,7 @@
 const track = document.getElementById('track');
 const images = document.querySelectorAll('.card');
 
+
 setInterval(() => {
     track.appendChild(track.firstElementChild);
-}, 5000);
+}, 2000);
